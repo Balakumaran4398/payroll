cb9272fd94e567c1aac5c92b354865e113d42e3b
Balakumaran4398 <balakumaran@ridsys.in>
2026-03-19 17:41:29 +0530

    Angular 11 upgrade completed, preparing for Angular 12

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -106,8 +106,9 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      preloadingStrategy: PreloadAllModules,
-    }),
+    preloadingStrategy: PreloadAllModules,
+    relativeLinkResolution: 'legacy'
+}),
   ],
   exports: [RouterModule],
 })
